b1e5623bccdd1d4672e4621ba026dd9ede714a19
Antoni <antonig@sas.upenn.edu>
2018-04-03 17:54:42 -0400

    having issues with timeouts

--- test_add_card_deckbox.js
+++ test_add_card_deckbox.js
@@ -15,8 +15,8 @@ var init = async function(){
   		await login(page);
   	}
   	else {
-  		console.log('logged in on deckbox.');
-      await add();
+  	  console.log('logged in on deckbox.');
+      await add(page);
   	}
   	await browser.close();
 };
@@ -25,29 +25,46 @@ async function login(page) {
 	console.log('attempting to hit login button');
   await page.click('#nav > ul > li:nth-child(2) > a');
   await page.screenshot({path: 'deckbox_login.png'});
-  var email;
-  var password;
-  (async () => {
-    await prompt.get(['email', 'password'], function (err, result) {
-      //
-      // akgdevelopment@gmail.com, TestPass1
-      //
-      console.log('Command-line input received:');
-      email = result.email;
-      password = result.password;
-
-      await page.$eval('#login', el => el.value = email);
-      await page.$eval('#password', el => el.value = password);
-      await page.screenshot({path: 'deckbox_login_form.png'});
-      await page.click('#submit_button');
-      await add();
-    });
-  })
+  // var email;
+  // var password;
+  // var flag;
+  // prompt.get(['email', 'password'], function (err, result) {
+  //   //
+  //   // akgdevelopment@gmail.com, TestPass1
+  //   //
+  //   console.log('Command-line input received:');
+  //   email = result.email;
+  //   password = result.password;
+  // });
+  await page.$eval('#login', el => el.value = 'akgdevelopment@gmail.com');
+  await page.$eval('#password', el => el.value = 'TestPass1');
+  await page.screenshot({path: 'deckbox_login_form.png'});
+  await page.click('#submit_button');
+  await page.waitForNavigation(0);
+  await add(page);
 }
 
-async function add() {
+async function add(page) {
   console.log('navigating to home page');
   await page.screenshot({path: 'deckbox_home.png'});
+  console.log('navigating to inventory page');
+  await page.click('#section_mtg > li:nth-child(1) > a');
+  await page.waitForNavigation(0);
+  await page.screenshot({path: 'deckbox_inventory.png'});
+  // console.log('attempting to adjust card settings');
+  // await page.click('#_button_default_edition_single');
+  // await page.waitForNavigation(0);
+  // await page.$eval('body > div:nth-child(23) > div > ul > li:nth-child(1) > div > input[type="text"]', el => el.value = 'alpha');
+  // await page.click('#tab_import_list > a');
+  // await page.waitForNavigation(0);
+  // await page.$eval('#new_card_list_txt_advanced', el => el.value = 'llanowar elves');
+  // await page.screenshot({path: 'deckbox_elf_pre_add.png'});
+  // console.log('attempting to add card');
+  // await page.click('#submit_add_list');
+  // await page.waitForNavigation(0);
+  // await page.screenshot({path: 'deckbox_elf_added.png'});
+  // console.log('added card');
 }
+
 
 module.exports.addElf = init;
