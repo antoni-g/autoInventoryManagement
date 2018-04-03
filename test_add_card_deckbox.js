49d6dc97591fcd72372d7859e1962c1944a37acf
Antoni <antonig@sas.upenn.edu>
2018-04-03 03:03:20 -0400

    got the hang of puppeteer

--- test_add_card_deckbox.js
+++ test_add_card_deckbox.js
@@ -1,21 +1,28 @@
 const puppeteer = require('puppeteer');
-var json = info('./test.json');
+var info = require('./test.json');
 
 // series of functions to test adding a llanowar elves to deckbox
-var login = async function(){
+var init = async function(){
 	console.log('attempting to add elf to deckbox.')
 	const browser = await puppeteer.launch();
   	const page = await browser.newPage();
   	await page.goto('https://deckbox.org');
-  	await page.screenshot({path: 'google.png'});
+  	await page.screenshot({path: 'deckbox_landing.png'});
   	var pageTitle = await page.title();
   	if (pageTitle === 'Deckbox') {
-  		console.log('user not logged in on deckbox.')
+  		console.log('user not logged in on deckbox.');
+  		await login(page);
   	}
   	else {
-  		console.log('logged in on deckbox.')
+  		console.log('logged in on deckbox.');
   	}
   	await browser.close();
 };
 
-module.exports.addElf = login;
+async function login(page) {
+	console.log('attempting to hit login button');
+  await page.click('#nav > ul > li:nth-child(2) > a');
+  await page.screenshot({path: 'deckbox_login.png'});
+}
+
+module.exports.addElf = init;
