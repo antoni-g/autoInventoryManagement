c2a700f6dc875dd392dcb15ab5bf5ff008ccb436
Antoni <antonig@sas.upenn.edu>
2018-04-03 02:46:01 -0400

    working on accessing deckbox

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log('run success');
+
+var test = require("./test_add_card_deckbox.js");
+
+test.addElf(); 
--- test_add_card_deckbox.js
+++ test_add_card_deckbox.js
@@ -1,2 +1,21 @@
 const puppeteer = require('puppeteer');
+var json = info('./test.json');
 
+// series of functions to test adding a llanowar elves to deckbox
+var login = async function(){
+	console.log('attempting to add elf to deckbox.')
+	const browser = await puppeteer.launch();
+  	const page = await browser.newPage();
+  	await page.goto('https://deckbox.org');
+  	await page.screenshot({path: 'google.png'});
+  	var pageTitle = await page.title();
+  	if (pageTitle === 'Deckbox') {
+  		console.log('user not logged in on deckbox.')
+  	}
+  	else {
+  		console.log('logged in on deckbox.')
+  	}
+  	await browser.close();
+};
+
+module.exports.addElf = login;
